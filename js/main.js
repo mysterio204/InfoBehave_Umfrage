8e30c357334f331e717db75d1a47d80a86e61dff
mysterio204 <thiemo.kersting@googlemail.com>
2016-06-22 17:34:30 +0200

    Umfrage
    bisher nur platzhalter-tweets

---
+++ js/main.js
@@ -0,0 +1,105 @@
+    console.log("Hello World");
+    var result_arr = [];
+    var counter = 1;
+    var FileNames = [];
+
+
+    var updateTextInput = function (val) {
+
+        document.getElementById('valueDisplay').innerHTML = val;
+
+        console.log(val);
+    };
+
+
+    var clickNext = function () {
+
+
+
+    };
+
+
+
+    var createTweetArray = function () {
+
+        for (var a = [], i = 1; i < 2; ++i) a[i] = i;
+
+        // http://stackoverflow.com/questions/962802#962890
+        function shuffle(array) {
+            var tmp, current, top = array.length;
+            if (top)
+                while (--top) {
+                    current = Math.floor(Math.random() * (top + 1));
+                    tmp = array[current];
+                    array[current] = array[top];
+                    array[top] = tmp;
+                }
+            return array;
+        }
+
+        a = shuffle(a);
+        return a;
+    };
+
+    var createFileArray = function (nums) {
+        var files = []
+        for (i = 1; i < 7; i++) {
+            var name1 = "t" + nums[0] + "s" + i + "o1.jpg";
+            var name2 = "t" + nums[0] + "s" + i + "o2.jpg";
+
+            files.push(name1);
+            files.push(name2);
+
+        }
+        return files;
+    };
+
+    var start_survey = function () {
+        $('#slide_container').show();
+         $('#prog_bar').show();
+        $('#start_container').hide();
+        nextTweet();
+
+    };
+
+    var nextTweet = function () {
+        console.log("Tweet number : " + counter);
+        var prev = {
+            image: $('#tweet_img').attr('src'),
+            score: $('#valueDisplay').text()
+        };
+        result_arr.push(prev);
+
+
+
+        if (counter <= 12) {
+            $('#tweet_img').attr("src", "/tweets/" + FileNames[counter - 1]);
+            
+            var percent = ((counter)/12)*100;
+            console.log("percent done: "+percent );
+             $('.progress-bar').css('width', percent+'%').attr('aria-valuenow', percent).text(counter+"/12"); 
+               counter++;
+        } else {
+
+            console.log(result_arr);
+            $('#slide_container').hide();
+            $('#end_container').show();
+            $('#tweet_img').hide();
+            $('#prog_bar').hide();
+        }
+
+
+
+
+
+    }
+
+    $(document).ready(function () {
+        $('#slide_container').hide();
+         $('#end_container').hide();
+         $('#prog_bar').hide();
+        var tweetNums = createTweetArray();
+
+        FileNames = createFileArray(tweetNums);
+        console.log(FileNames);
+    });
